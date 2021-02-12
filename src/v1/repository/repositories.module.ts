f993089109eee276e51290b54d26c61352b91c0a
Zakhar Idzynskyi <zakhar.idzinsky@scotts.com>
2021-02-12 21:26:52 +0200

    CHANGE static versionControlProvider to dynamic

--- src/v1/repository/repositories.module.ts
+++ src/v1/repository/repositories.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RepositoriesController } from './controllers/repositories.controller';
-import { VersionControlProviderEnum } from './enum';
 import { GitHubProviderService } from './services/github-provider.service';
 import { RepositoriesService } from './services/repositories.service';
-
 @Module({
   controllers: [RepositoriesController],
   providers: [
     {
       provide: 'VersionControlProvider',
-      useValue: VersionControlProviderEnum.GitHub,
+      useFactory: () => process.env.NODE_ENV,
     },
     RepositoriesService,
     GitHubProviderService,
